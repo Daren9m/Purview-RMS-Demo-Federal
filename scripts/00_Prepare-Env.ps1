c0f3bd85edd93ffc5209abe6bf04db1251db36b7
Daren Maranya <46686703+Daren9m@users.noreply.github.com>
2025-08-21 17:35:46 -0600

    Refined authentication to allow for manual interaction

--- scripts/00_Prepare-Env.ps1
+++ scripts/00_Prepare-Env.ps1
@@ -3,5 +3,12 @@ param(
   [string] $Cloud = 'Global'
 )
 
-$graphContext = . .\scripts\utils\Connect-Graph.ps1 -Environment $Cloud
+# Import-Module Microsoft.Graph -ErrorAction Stop
+# Import Microsoft.Graph.Beta.* modules as needed for beta cmdlets.
+# Use device code authentication to avoid dependency on a local browser or cached credentials.
+Connect-MgGraph -Environment $Cloud -UseDeviceCode -NoWelcome
+
+$graphContext = Get-MgContext
+
+
 . .\scripts\utils\Connect-Compliance.ps1 -Environment $Cloud
--- scripts/utils/Connect-Graph.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [ValidateSet('Global','USGov')]
-  [string] $Environment = 'Global'
-)
-# Import-Module Microsoft.Graph -ErrorAction Stop
-# Import Microsoft.Graph.Beta.* modules as needed for beta cmdlets.
-# Use device code authentication to avoid dependency on a local browser or cached credentials.
-Connect-MgGraph -Environment $Environment -UseDeviceCode -NoWelcome
-Get-MgContext
