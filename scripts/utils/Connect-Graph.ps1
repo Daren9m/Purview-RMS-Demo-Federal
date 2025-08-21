cf45e638f7a63b2bcb6bc2b826008327dad5ecb0
Daren9m <46686703+Daren9m@users.noreply.github.com>
2025-08-21 17:23:05 -0600

    Comment out Microsoft.Graph module import

--- scripts/utils/Connect-Graph.ps1
+++ scripts/utils/Connect-Graph.ps1
@@ -2,7 +2,7 @@ param(
   [ValidateSet('Global','USGov')]
   [string] $Environment = 'Global'
 )
-Import-Module Microsoft.Graph -ErrorAction Stop
+# Import-Module Microsoft.Graph -ErrorAction Stop
 # Import Microsoft.Graph.Beta.* modules as needed for beta cmdlets.
 # Use device code authentication to avoid dependency on a local browser or cached credentials.
 Connect-MgGraph -Environment $Environment -UseDeviceCode -NoWelcome
