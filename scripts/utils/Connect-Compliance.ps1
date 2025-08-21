2b7be094c9e135a81cb4d899b3718d546cb60c78
Daren9m <46686703+Daren9m@users.noreply.github.com>
2025-08-21 16:46:39 -0600

    Prompt for user email without clearing sessions

--- scripts/utils/Connect-Compliance.ps1
+++ scripts/utils/Connect-Compliance.ps1
@@ -3,8 +3,10 @@ param(
   [string] $Environment = 'Global'
 )
 Import-Module ExchangeOnlineManagement -ErrorAction Stop
+# Prompt for a user account to open an interactive authentication window.
+$account = Read-Host 'Enter the UPN for authentication'
 if ($Environment -eq 'USGov') {
-  Connect-IPPSSession -ConnectionUri 'https://ps.compliance.protection.office365.us/powershell-liveid/' -AzureADAuthorizationEndpointUri 'https://login.microsoftonline.us/common' | Out-Null
+  Connect-IPPSSession -UserPrincipalName $account -ConnectionUri 'https://ps.compliance.protection.office365.us/powershell-liveid/' -AzureADAuthorizationEndpointUri 'https://login.microsoftonline.us/common' | Out-Null
 } else {
-  Connect-IPPSSession | Out-Null
+  Connect-IPPSSession -UserPrincipalName $account | Out-Null
 }
